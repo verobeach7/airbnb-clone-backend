a30a14e2ea7be984f2bc1321b2f6dc210ab96e65
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-24 15:20:16 +0900

    AirBnb Clone Backend: URLs and Views(config/urls.py, views.py: urlpatterns)

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,47 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+# ### 방법 1. 이 경우 메소드명이 중복되어 문제가 될 수 있음
+# from rooms.views import say_hello
+# from users.views import say_hello
+
+# urlpatterns = [
+#     # url에 admin으로 접근하면 admin.site.urls 함수를 작동시킴
+#     path("admin/", admin.site.urls),
+#     path("rooms", say_hello),
+#     path("users", say_hello),
+# ]
+
+# ### 방법 2. 이 경우 views라는 파일명이 중복되게 됨
+# from rooms import views
+# from users import views
+
+# urlpatterns = [
+#     # url에 admin으로 접근하면 admin.site.urls 함수를 작동시킴
+#     path("admin/", admin.site.urls),
+#     path("rooms", views.say_hello),
+#     path("users", views.say_hello),
+# ]
+
+# ### as를 이용하여 별칭을 붙여 해결 가능
+# from rooms import views as rooms_views
+# from users import views as users_views
+
+# urlpatterns = [
+#     # url에 admin으로 접근하면 admin.site.urls 함수를 작동시킴
+#     path("admin/", admin.site.urls),
+#     path("rooms", rooms_views.say_hello),
+#     path("users", users_views.say_hello),
+# ]
+
+from rooms import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # 여기에 path를 넣어줘야만 장고가 해당 url에 따른 함수를 실행하여 view를 보여줌
+    path("rooms", views.say_hello),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def say_hello(request):
+    print(
+        "request", request
+    )  # 요청하고 있는 사용자 및 브라우저 정보, 전송하고 있는 데이터, 요청한 url 정보, ip정보, 쿠키 등을 담고 있음
+    # return "hello"  # Error: 'str' object has no attribute 'get' -> HTTP Response를 Return해야 함
+    return HttpResponse("hello")
