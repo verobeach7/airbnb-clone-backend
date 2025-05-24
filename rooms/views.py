338d67c2d51897159d6cfe3e6b8b4b2da2ce88ae
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-24 17:33:36 +0900

    AirBnb Clone Backend: URLs and Views(Render)

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 # Create your views here.
 def see_all_rooms(request):
-    return HttpResponse("see all rooms")
+    rooms = Room.objects.all()
+    # render의 첫 번째 인자: request
+    # render의 두 번째 인자: 템플릿
+    # render의 세 번째 인자: context - html 페이지로 보낼 데이터(딕셔너리 형태의 {Key: Value}로 보냄)
+    return render(
+        request,
+        "all_rooms.html",
+        {"rooms": rooms, "title": "this title comes from django"},
+    )
 
 
 # url에서 parameter를 받으므로 받아 줄 공간을 만들어야 함. room_id 추가
