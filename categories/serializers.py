d8d36e8e89026623a9f8d50bf83ec023cea9fdec
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-27 15:45:51 +0900

    AirBnb Clone Backend: Django REST Framework(Serializer)

---
+++ categories/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.Serializer):
+    # 모델에 있는 속성을 다시 알려줘서 JSON으로 표현할 수 있도록 함
+    # 모델에도 작성한 것을 다시 반복적으로 작성해야 해서 비효율적
+    # 추후에 Django REST Framework를 사용하여 쉽게 해결할 것임
+
+    # 여기에 작성한 것만 Serialize해서 JSON으로 보냄
+    # pk = serializers.IntegerField()
+    pk = (
+        serializers.CharField()
+    )  # 스트링으로 변환하여 보내게 됨. 즉, 원하는 형태로 데이터 타입도 수정 가능
+    name = serializers.CharField(required=True)
+    kind = serializers.CharField()
+    created_at = serializers.DateTimeField()
--- categories/views.py
+++ categories/views.py
@@ -1,14 +1,26 @@
 from .models import Category
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CategorySerializer
 
 
 # Django REST Framework를 사용하기 위해서 데코레이터만 달아주면 됨
 @api_view()
 def categories(request):
+    all_categories = Category.objects.all()
+    # 그냥 all_categories만 넘기면 serializers.py에서는 하나의 category만 넘어오고 이것을 번역할 것이라고 생각함. 실은 여러 카테고리를 담고 있는 리스트가 보내지기 때문에 `many=True`를 함께 보내줘야 함.
+    serializer = CategorySerializer(
+        all_categories,
+        many=True,
+    )
     return Response(
         {
             "ok": True,
-            "categories": Category.objects.all(),
+            # Response가 자동으로 Serializable하게 바꿔주지는 않음
+            # Django Serialization Framework는 별로 좋지 않음
+            # Django REST Framework와 함께 제공되는 Serializer를 사용하여 해결
+            # 별도의 serializers.py 파일 생성 필요
+            # "categories": Category.objects.all(),
+            "categories": serializer.data,
         }
     )
