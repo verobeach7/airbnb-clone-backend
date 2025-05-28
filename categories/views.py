e6309ad88156b44ddf4628ccf324ee52c09ee91d
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-28 15:25:34 +0900

    AirBnb Clone Backend: Django REST Framework(delete())

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,7 @@
 from .models import Category
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.exceptions import NotFound
 from .serializers import CategorySerializer
 
@@ -39,7 +40,7 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)  # Django Model의 Instance를 가져옴
@@ -68,3 +69,6 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     # url에서 rooms로 접근하면 rooms/urls.py가 view를 처리
     # include를 이용하면 별도의 import가 불필요함
     # 파일 경로를 넣어주면 장고가 알아서 처리함
-    path("rooms/", include("rooms.urls")),
-    path("categories/", include("categories.urls")),
+    # API 버져닝을 통해 관리: 추후 개선 작업 시 기존과 새로운 버전을 모두 활용 가능
+    # 서버 작업과 API를 분리 가능
+    path("api/v1/rooms/", include("rooms.urls")),
+    path("api/v1/categories/", include("categories.urls")),
 ]
