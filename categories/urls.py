134de9c5f2f9c1dc4242195561768e2b720409f0
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-29 16:40:31 +0900

    AirBnb Clone Backend: Django REST Framework(Magic: ModelViewSet on views.py)

--- categories/urls.py
+++ categories/urls.py
@@ -6,6 +6,26 @@ urlpatterns = [
     # 클래스를 이용하는 것으로 변경해줘야 함
     # .as_view()를 붙이는 것은 단지 클래스를 가져오기 위한 규칙일 뿐임
     # .as_view()가 하는 일은 get과 post에 따라 Method를 작동시키기 위한 것
-    path("", views.Categories.as_view()),
-    path("<int:pk>", views.CategoryDetail.as_view()),
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                # ViewSet의 Actions Method와 사용자가 보낼 HTTP Method를 연결해주면 됨
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        # 반드시 변수명을 'pk'로 해줘야 함
+        # ModelViewSet 클래스의 메소드를 보면 이미 'pk'로 사전 설정되어 있음
+        "<int:pk>",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
 ]
--- categories/views.py
+++ categories/views.py
@@ -4,61 +4,10 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.exceptions import NotFound
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .serializers import CategorySerializer
 
 
-# Django REST Framework의 APIView Class 사용
-# 이 클래스에는 @api_view, if request.method를 이미 가지고 있음
-# 클래스를 이용하면 if문을 이용하여 분기하지 않아도 됨
-class Categories(APIView):
-    # get, post method만 만들었기 때문에 알아서 둘만 작동함
-    def get(self, request):
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(
-            all_categories,
-            many=True,
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(CategorySerializer(new_category).data)
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-
-    # get_object()를 만들어서 get, put, delete에서 모두 사용할 수 있게 함
-    # get_object()를 만드는 것이 규칙은 아님. 단지 DRF를 사용하는 관습, 관례.
-    def get_object(self, pk):
-        try:
-            category = Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise NotFound
-        return category
-
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        print(
-            serializer
-        )  # ModelSerializer를 사용해서 얻을 수 있는 serializer를 볼 수 있음
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk),
-            data=request.data,
-            partial=True,
-        )
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
