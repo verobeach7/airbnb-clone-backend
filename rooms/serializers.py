4bd7ad0815b60e951dafcbec8b5fc418686c3a62
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-30 15:57:31 +0900

    AirBnb Clone Backend: REST API(Rooms: depth=1 on serializers.py)

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Amenity
+from .models import Amenity, Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+        # 관계를 확장하여 Primary Key가 아닌 실제 객체(Object)를 보여지게 할 수 있음
+        depth = 1
 
 
 class AmenitySerializer(ModelSerializer):
--- rooms/urls.py
+++ rooms/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views  # 현재 폴더 내에서 views.py를 import
 
 urlpatterns = [
+    path("", views.Rooms.as_view()),
     path("amenities/", views.Amenities.as_view()),
     path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
