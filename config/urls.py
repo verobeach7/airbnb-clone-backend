6713ddaf2fd8881be3bd1a7a9334ee6300b0405c
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-30 15:28:29 +0900

    AirBnb Clone Backend: REST API(Perks, PerkDetail: urls, models, serializers, views)

--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     # 서버 작업과 API를 분리 가능
     path("api/v1/rooms/", include("rooms.urls")),
     path("api/v1/categories/", include("categories.urls")),
+    path("api/v1/experiences/", include("experiences.urls")),
 ]
