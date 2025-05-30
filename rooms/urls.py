ac366f3031d5cfa89d87ec8087425cef5f9d6b16
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-05-30 13:37:53 +0900

    AirBnb Clone Backend: REST API(All Amenities, AmenityDetail: Practice)

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views  # 현재 폴더 내에서 views.py를 import
 
 urlpatterns = [
-    # "": 이미 config/urls.py에서 rooms 경로로 들어와 있기 때문에 ""은 rooms의 root에 해당
-    path("", views.see_all_rooms),
-    # url에서 변수 받는 법: 변수가 들어가는 자리에 <Type:parameter_name>을 넣어주면 됨
-    path("<int:room_pk>", views.see_one_room),
+    path("amenities/", views.Amenities.as_view()),
+    path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,33 +1,98 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .models import Amenity
+from .serializers import AmenitySerializer
 
 
-# Create your views here.
-def see_all_rooms(request):
-    rooms = Room.objects.all()
-    # render의 첫 번째 인자: request
-    # render의 두 번째 인자: 템플릿
-    # render의 세 번째 인자: context - html 페이지로 보낼 데이터(딕셔너리 형태의 {Key: Value}로 보냄)
-    return render(
-        request,
-        "all_rooms.html",
-        {"rooms": rooms, "title": "this title comes from django"},
-    )
-
-
-# url에서 parameter를 받으므로 받아 줄 공간을 만들어야 함. room_id 추가
-def see_one_room(request, room_pk):
-    try:
-        room = Room.objects.get(pk=room_pk)
-        return render(
-            request,
-            "room_detail.html",
-            {"room": room},
+class Amenities(APIView):
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(
+            all_amenities,
+            many=True,
         )
-    except Room.DoesNotExist:
-        return render(
-            request,
-            "room_detail.html",
-            {"not_found": True},
+        print(serializer.data)
+        # [{'id': 1, 'created_at': '2025-05-17T11:59:41.156367+09:00', 'updated_at': '2025-05-17T11:59:41.156382+09:00', 'name': '여행 가방 보관 가능', 'description': '체크인 시간보다 일찍 도착하거나 체크아웃 후 짐 보관을 원하는 게스트를 위한 서비스'}, {'id': 2, 'created_at': '2025-05-17T12:00:08.629960+09:00', 'updated_at': '2025-05-17T12:00:08.629986+09:00', 'name': '장기 숙박 가능', 'description': '28일 이상 숙박 가능'}, {'id': 3, 'created_at': '2025-05-17T12:00:57.920259+09:00', 'updated_at': '2025-05-30T13:28:09.479454+09:00', 'name': '디지털도어록', 'description': '게스트가 현관문 비밀번호를 입력하여 직접 체크인합니다'}]
+        return Response(serializer.data)
+
+    def post(self, request):
+        print("request.data", request.data)
+        # request.data {'name': '멋진 뒷뜰', 'description': '꽃밭에서 놀아요'}
+        serializer = AmenitySerializer(data=request.data)
+        print("serializer", serializer)
+        # serializer AmenitySerializer(data={'name': '멋진 뒷뜰', 'description': '꽃밭에서 놀아요'}):
+        # id = IntegerField(label='ID', read_only=True)
+        # created_at = DateTimeField(read_only=True)
+        # updated_at = DateTimeField(read_only=True)
+        # name = CharField(max_length=150)
+        # description = CharField(allow_blank=True, allow_null=True, max_length=150, required=False)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            print("amenity", amenity)
+            ### Python Object
+            # amenity 멋진 뒷뜰
+            return Response(AmenitySerializer(amenity).data)
+        else:
+            return Response(serializer.errors)
+
+
+class AmenityDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Amenity.objects.get(pk=pk)
+        except Amenity.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        amenity = self.get_object(pk)
+        print("amenity", amenity)
+        ### Python Object
+        # amenity 멋진 도어록
+        serializer = AmenitySerializer(amenity)
+        print("serializer", serializer)
+        # serializer AmenitySerializer(<Amenity: 멋진 도어록>):
+        # id = IntegerField(label='ID', read_only=True)
+        # created_at = DateTimeField(read_only=True)
+        # updated_at = DateTimeField(read_only=True)
+        # name = CharField(max_length=150)
+        # description = CharField(allow_blank=True, allow_null=True, max_length=150, required=False)
+        print("serializer.data", serializer.data)
+        # JSON 변환됨
+        # serializer.data {'id': 3, 'created_at': '2025-05-17T12:00:57.920259+09:00', 'updated_at': '2025-05-30T13:20:35.069248+09:00', 'name': '멋진 도어록', 'description': '게스트가 현관문 비밀번호를 입력하여 직접 체크인합니다'}
+        return Response(serializer.data)
+        # return Response(
+        #     AmenitySerializer(self.get_object(pk)).data,
+        # )
+
+    def put(self, request, pk):
+        amenity = self.get_object(pk)
+        serializer = AmenitySerializer(
+            amenity,
+            data=request.data,
+            partial=True,
         )
+        print("serializer", serializer)
+        # serializer AmenitySerializer(<Amenity: 멋진 도어록>, data={'name': '디지털도어록'}, partial=True):
+        # id = IntegerField(label='ID', read_only=True)
+        # created_at = DateTimeField(read_only=True)
+        # updated_at = DateTimeField(read_only=True)
+        # name = CharField(max_length=150)
+        # description = CharField(allow_blank=True, allow_null=True, max_length=150, required=False)
+        if serializer.is_valid():
+            updated_amenity = serializer.save()
+            print("updated_amenity", updated_amenity)
+            # Python Object
+            # updated_amenity 멋진 도어록
+            print("after serializing", AmenitySerializer(updated_amenity).data)
+
+            # after serializing {'id': 3, 'created_at': '2025-05-17T12:00:57.920259+09:00', 'updated_at': '2025-05-30T13:20:35.069248+09:00', 'name': '멋진 도어록', 'description': '게스트가 현관문 비밀번호를 입력하여 직접 체크인합니다'}
+            return Response(AmenitySerializer(updated_amenity).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        amenity = self.get_object(pk)
+        amenity.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
