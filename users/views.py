8ff06f1ddf8e810e41831d98174d5d51c365865b
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-06-05 14:58:26 +0900

    AirBnb Clone Backend: Users API(Create User: .set_password())

--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 # status 자체를 import하면 status를 타이핑하면 자동완성으로 한번에 볼 수 있음
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
 from . import serializers
 
 
@@ -16,15 +17,41 @@ class Me(APIView):
         serializer = serializers.PrivateUserSerializer(user)
         return Response(serializer.data)
 
-    def post(self, request):
+    def put(self, request):
         user = request.user
+        # Serializer에서 별도의 validation이 필요하지 않음(비밀번호 수정은 다른 url에서 처리)
         serializer = serializers.PrivateUserSerializer(
             user,
             data=request.data,
             partial=True,
         )
+        # DRF의 ModelSerializer는 Uniqueness 검증이 이미 포함되어 있음
+        # 검증해야 하는 유일한 것은 누군가가 User를 만들 때 password를 설정하는 것만 하면 됨
         if serializer.is_valid():
             user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        # 사용자가 계정을 생성하기 위해 request를 보내면 그 request에 password가 있는지 검증해야 함
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # # 아래처럼 하는 것은 password를 암호화하지 않고 그대로 저장하는 것
+            # # 절대로 이렇게 해서는 안됨
+            # user.password = password
+
+            # User Model의 .set_password() 메서드를 사용
+            # .set_password() 메서드는 password를 해쉬화 하여 저장
+            user.set_password(password)
+            user.save()
             serializer = serializers.PrivateUserSerializer(user)
             return Response(serializer.data)
         else:
