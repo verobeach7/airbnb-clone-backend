2757680475d7ecd1d393911cf5dda915c3b0eb85
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-06-09 19:46:08 +0900

    AirBnb Clone Backend: GraphQL API(Paginated Relationship: make method field with arguments)

--- rooms/types.py
+++ rooms/types.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 import strawberry
 from strawberry import auto
 import typing
@@ -19,8 +20,20 @@ class RoomType:
     # 모든 field의 이름은 Room Model에 있는 이름과 같아야 함
     # 관계를 연결해 줄 때는 ""를 이용하여 적어줘야 함. 당연히 UserType을 임포트해야 함
     owner: "UserType"
-    reviews: typing.List["ReviewType"]
+    # reviews: typing.List["ReviewType"]
+
+    ### Pagination
+    # 위의 기본 Resolver 대신에 나만의 Resolver 생성
+    @strawberry.field
+    # self는 rooms, 원하는 인자를 얼마든지 받을 수 있음
+    # page 인자를 여기서 요청하면 알아서 전달해 줌
+    def reviews(self, page: int) -> typing.List["ReviewType"]:
+        page_size = settings.PAGE_SIZE
+        start = (page - 1) * page_size
+        end = start + page_size
+        return self.reviews.all()[start:end]
 
     @strawberry.field
-    def potato(self) -> str:
-        return "lalalla"
+    # self에는 rooms가 들어옴
+    def rating(self) -> str:
+        return self.rating()
