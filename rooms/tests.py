9e176b5ec12bb2a5808ca55e3ee2e24755d47eb0
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-06-12 16:12:20 +0900

    AirBnb Clone Backend: API Testing(Create Amenities Test: POST Request)

--- rooms/tests.py
+++ rooms/tests.py
@@ -9,6 +9,7 @@ from rooms import models
 class TestAmenities(APITestCase):
     NAME = "Amenity Test"
     DESC = "Amenity Description"
+    URL = "/api/v1/rooms/amenities/"
 
     # setUp 메서드는 다른 모든 test들이 실행되기 전에 실행됨
     # 여기서 DB를 생성하고 테스트를 진행할 수 있음
@@ -24,7 +25,7 @@ class TestAmenities(APITestCase):
     def test_all_amenities(self):
         # client는 API Client를 말하는 것으로 API 서버로 request를 보낼 수 있게 함
         # URL을 넣어주면 마치 브라우저에서 접근하는 것 처럼 request를 전송
-        response = self.client.get("/api/v1/rooms/amenities/")
+        response = self.client.get(self.URL)
         data = response.json()
 
         # data가 없기 때문에 데이터 없이 테스트 할 수 있는 것들을 테스트 해야 함
@@ -40,3 +41,45 @@ class TestAmenities(APITestCase):
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0]["name"], self.NAME)
         self.assertEqual(data[0]["description"], self.DESC)
+
+    def test_create_amenity(self):
+        new_amenity_name = "New Amenity"
+        new_amenity_description = "New Amenity Description"
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
+        )
+        data = response.json()
+        # print(data)  # {'name': 'New Amenity', 'description': 'New Amenity Description'}
+        # print(type(data))  # <class 'dict'>
+        # print(data["name"])  # New Amenity
+        # print(data.get("name"))  # New Amenit: 더 안전한 방식!
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
+
+        self.assertEqual(
+            data["name"],
+            new_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            new_amenity_description,
+        )
+
+        # 데이터 없이 POST Request를 보내는 경우: name과 description 모두 없음
+        response = self.client.post(self.URL)
+        data = response.json()
+
+        # 데이터 없이 보내면 name은 필수 입력이므로 에러가 발생
+        # 데이터가 없으면 400 응답을 잘 보내는지 확인
+        self.assertEqual(response.status_code, 400)
+        # 에러 메시지 안에 name이라는 키워드가 있는지 확인
+        self.assertIn("name", data)
--- rooms/views.py
+++ rooms/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.db import transaction
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 from rest_framework.response import Response
 from rest_framework.exceptions import (
     NotFound,
@@ -31,15 +31,15 @@ class Amenities(APIView):
             all_amenities,
             many=True,
         )
-        print(serializer.data)
+        # print(serializer.data)
         # [{'id': 1, 'created_at': '2025-05-17T11:59:41.156367+09:00', 'updated_at': '2025-05-17T11:59:41.156382+09:00', 'name': '여행 가방 보관 가능', 'description': '체크인 시간보다 일찍 도착하거나 체크아웃 후 짐 보관을 원하는 게스트를 위한 서비스'}, {'id': 2, 'created_at': '2025-05-17T12:00:08.629960+09:00', 'updated_at': '2025-05-17T12:00:08.629986+09:00', 'name': '장기 숙박 가능', 'description': '28일 이상 숙박 가능'}, {'id': 3, 'created_at': '2025-05-17T12:00:57.920259+09:00', 'updated_at': '2025-05-30T13:28:09.479454+09:00', 'name': '디지털도어록', 'description': '게스트가 현관문 비밀번호를 입력하여 직접 체크인합니다'}]
         return Response(serializer.data)
 
     def post(self, request):
-        print("request.data", request.data)
+        # print("request.data", request.data)
         # request.data {'name': '멋진 뒷뜰', 'description': '꽃밭에서 놀아요'}
         serializer = AmenitySerializer(data=request.data)
-        print("serializer", serializer)
+        # print("serializer", serializer)
         # serializer AmenitySerializer(data={'name': '멋진 뒷뜰', 'description': '꽃밭에서 놀아요'}):
         # id = IntegerField(label='ID', read_only=True)
         # created_at = DateTimeField(read_only=True)
@@ -48,12 +48,15 @@ class Amenities(APIView):
         # description = CharField(allow_blank=True, allow_null=True, max_length=150, required=False)
         if serializer.is_valid():
             amenity = serializer.save()
-            print("amenity", amenity)
+            # print("amenity", amenity)
             ### Python Object
             # amenity 멋진 뒷뜰
             return Response(AmenitySerializer(amenity).data)
         else:
-            return Response(serializer.errors)
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
 
 
 class AmenityDetail(APIView):
