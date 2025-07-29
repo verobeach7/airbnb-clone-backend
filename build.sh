75d44c0dc5ba6af7b6d2283a12b2b3042eab7efa
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-07-29 16:54:12 +0900

    AirBnb Clone Backend: Deployment(Render: createsuperuser without Shell #2)

--- build.sh
+++ build.sh
@@ -15,7 +15,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Render의 Shell을 사용하지 않고 Superuser 만들기
-if [[ $CREATE_SUPERUSER]];
-then
+if [ $CREATE_SUPERUSER ]; then
     python manage.py createsuperuser --no-input
 fi
