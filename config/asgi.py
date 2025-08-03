68b7b5c99e8ad4ff727876884a5fb09bcc8ccd74
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2025-08-03 15:55:08 +0900

    AirBnb Clone Backend: Deployment(lifespan off on render.yaml #5)

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,20 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_asgi_application()
+
+from uvicorn.workers import UvicornWorker
+
+
+class DjangoUvicornWorker(UvicornWorker):
+    """
+    Generate UvicornWorker with lifespan='off', because Django does not
+    (and probably will not https://code.djangoproject.com/ticket/31508)
+    support Lifespan.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.config.lifespan = "off"
